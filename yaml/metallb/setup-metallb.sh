327d2f9313ff42c135e9afbdef471b7daf4b2dbf
Vitalie Mudrenco <xvitcoder@gmail.com>
2020-09-02 17:10:19 +0300

    Updated yaml files to helm

--- yaml/metallb/setup-metallb.sh
+++ yaml/metallb/setup-metallb.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-# MetalLB version
-VERSION='v0.9.3'
-
 # Create metallb namespace
 echo "[TASK 1] Create metallb namespace"
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/$VERSION/manifests/namespace.yaml
+kubectl create namespace metallb-system
 
-echo "[TASK 2] Apply metallb deployment"
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/$VERSION/manifests/metallb.yaml
-
-echo "[TASK 3] Generate metallb secret key"
-kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-
-echo "[TASK 4] Apply metallb ConfigMap"
+# Create metallb config map
+echo "[TASK 2] Create metallb configmap"
 kubectl apply -f `dirname $0`/metallb-config-map.yaml
+
+# Install metallb helm chart
+echo "[TASK 3] Install metallb helm chart"
+helm install metallb bitnami/metallb --set existingConfigMap=metallb-config --namespace metallb-system
---
+++ yaml/nfs-automatic-provisioner/setup-nfs-client-provisioner.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NFS_SERVER_IP="192.168.0.10"
+NFS_SERVER_PATH="/srv/nfs/kubedata"
+
+# Install nfs-client-provisioner helm chart
+echo "[TASK 1] Install nfs-client-provisioner helm chart"
+helm install nfs-client-provisioner stable/nfs-client-provisioner \
+        --set nfs.server=$NFS_SERVER_IP \
+        --set nfs.path=$NFS_SERVER_PATH \
+        --set storageClass.defaultClass=true
